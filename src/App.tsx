e9b07081ec93db8cca99a7ede6163b660176a3c4
Samuel Gratzl <samuel_gratzl@gmx.at>
2019-06-07 22:45:50 +1000

    change react import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import {observer, inject} from 'mobx-react';
 import {IWithStore} from './stores/interfaces';
 import {withStyles, createStyles, Theme, WithStyles} from '@material-ui/core/styles';
